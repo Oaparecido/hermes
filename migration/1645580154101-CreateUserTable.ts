36c2149f910412bd7cbb8025053e9b89b1d3921c
oaparecido <daniel.souza@quintoandar.com>
2022-02-22 23:19:00 -0300

    :card_file_box: Add migrations and file to config DB connection

---
+++ migration/1645580154101-CreateUserTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTable1645580154101 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isGenerated: true,
+            isPrimary: true,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user', true);
+  }
+}
