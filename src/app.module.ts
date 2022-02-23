567f4791df10afeb8e33fea54d51e2ec33741075
oaparecido <daniel.souza@quintoandar.com>
2022-02-22 23:16:59 -0300

    :tada: Begin a project in nest

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class AppModule {}
