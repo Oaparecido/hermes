0e50326ac744aa8369b4a672700696c519a14fa9
oaparecido <daniel.souza@quintoandar.com>
2022-03-24 20:32:23 -0300

    :card_file_box: Add new entity
    Add new folder with all configs to address

---
+++ src/address/entities/address.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  street: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  created_at: string;
+}
