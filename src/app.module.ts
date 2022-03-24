327ea6e13aea11f52c587cac81ed694cba3583d4
oaparecido <daniel.souza@quintoandar.com>
2022-03-24 20:47:04 -0300

    :wrench: Update file app.module
    :x

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
     UsersModule,
+    AddressModule,
     TypeOrmModule.forRoot({
       autoLoadEntities: true,
     }),
